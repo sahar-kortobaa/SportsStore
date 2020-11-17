54ec8fcbf990cf9a6078a603fcf36c69f0b96e92
sahar-kortobaa <sahar@kortobaa.com>
2020-11-17 20:30:57 +0200

    I mplementing Authentication

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -4,6 +4,8 @@ import { Observable } from "rxjs";
 import { Product } from "./product.model";
 import { Cart } from "./cart.model";
 import { Order } from "./order.model";
+import { map } from "rxjs/operators";
+
 
 const PROTOCOL = "http";
 const PORT = 3500;
@@ -11,13 +13,27 @@ const PORT = 3500;
 @Injectable()
 export class RestDataSource {
     baseUrl: string;
+    auth_token: string;
+
+
     constructor(private http: HttpClient) {
         this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
     }
+    
     getProducts(): Observable<Product[]> {
         return this.http.get<Product[]>(this.baseUrl + "products");
     }
+
     saveOrder(order: Order): Observable<Order> {
         return this.http.post<Order>(this.baseUrl + "orders", order);
+    }
+
+    authenticate(user: string, pass: string): Observable<boolean> {
+        return this.http.post<any>(this.baseUrl + "login", {
+            name: user, password: pass
+        }).pipe(map(response => {
+            this.auth_token = response.success ? response.token : null;
+            return response.success;
+        }));
     }
 }

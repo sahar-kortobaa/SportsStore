f2d884728f01dcd1ea48672eceb9a51c2b8790fd
sahar-kortobaa <sahar@kortobaa.com>
2020-11-18 14:44:54 +0200

    Extending the Data Source and Repositories

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -5,6 +5,7 @@ import { Product } from "./product.model";
 import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { map } from "rxjs/operators";
+import { HttpHeaders } from '@angular/common/http';
 
 
 const PROTOCOL = "http";
@@ -35,5 +36,42 @@ export class RestDataSource {
             this.auth_token = response.success ? response.token : null;
             return response.success;
         }));
+    }
+
+    saveProduct(product: Product): Observable<Product> {
+        return this.http.post<Product>(this.baseUrl + "products",
+            product, this.getOptions());
+    }
+
+    updateProduct(product): Observable<Product> {
+        return this.http.put<Product>(`${this.baseUrl}products/${product.id}`,
+            product, this.getOptions());
+    }
+
+    deleteProduct(id: number): Observable<Product> {
+        return this.http.delete<Product>(`${this.baseUrl}products/${id}`,
+            this.getOptions());
+    }
+
+    getOrders(): Observable<Order[]> {
+        return this.http.get<Order[]>(this.baseUrl + "orders", this.getOptions());
+    }
+
+    deleteOrder(id: number): Observable<Order> {
+        return this.http.delete<Order>(`${this.baseUrl}orders/${id}`,
+            this.getOptions());
+    }
+
+    updateOrder(order: Order): Observable<Order> {
+        return this.http.put<Order>(`${this.baseUrl}orders/${order.id}`,
+            order, this.getOptions());
+    }
+
+    private getOptions() {
+        return {
+            headers: new HttpHeaders({
+                "Authorization": `Bearer<${this.auth_token}>`
+            })
+        }
     }
 }
